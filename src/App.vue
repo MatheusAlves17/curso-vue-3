3ed8d9636e2512f9d7b846c17c00b41c88a71307
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-31 09:14:32 -0300

    feat: composition API reatividade

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
-  <p>{{ name }}</p>
+  <p>{{ user.first_name }} {{ user.last_name }}</p>
+  <p>{{ admin.first_name }} {{ admin.last_name }}</p>
   <button @click="mudarNome()">Alterar</button>
 </template>
 
 <script>
-
+import { reactive, ref } from 'vue';
 export default {
   name: 'App',
   components: {
   },
   setup(){
-    let name = 'Matt'
+    let user = reactive({
+      first_name: 'Matt',
+      last_name: 'Alves'
+    })
+    let admin = ref({
+      first_name: 'Admin',
+      last_name: 'Master'
+
+    })
     const mudarNome = () => {
-      name = 'Jon'
+      user.first_name = 'Matheus'
+      admin.value.last_name = 'Disabled'
     }
 
     return {
-      name,
-      mudarNome
+      mudarNome,
+      user,
+      admin
     }
   }
 }
