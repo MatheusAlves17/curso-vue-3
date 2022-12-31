c71f71d674ca8f16a0fa9ca8b1683e861ec1a5d3
Matheus Alves <matheusfrancisco0330@gmail.com>
2022-12-31 08:55:10 -0300

    feat: composition API setup

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,24 @@
 <template>
-  <AppProducts/>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-  <p>{{ $store.state.user.first_name }} {{ $store.state.user.last_name }}</p>
-  <p> {{ $store.state.user.email }} </p>
-  <button @click="updateUser()">Update</button>
+  <p>{{ name }}</p>
+  <button @click="mudarNome()">Alterar</button>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import AppProducts from './components/Products/AppProducts.vue';
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld,
-    AppProducts
-},
-  methods:{
-    updateUser(){
-      let newUser = {
-        first_name: 'Matt',
-        last_name: 'Alves',
-        email: 'matt@alves.com'
-      }
-      // this.$store.commit('updateUser', newUser)
-      this.$store.dispatch('storeUser', newUser).then(() => {
-        console.log('Salvo com sucesso!')
-      })
-    },
-    
   },
-  created(){
+  setup(){
+    let name = 'Matt'
+    const mudarNome = () => {
+      name = 'Jon'
+    }
+
+    return {
+      name,
+      mudarNome
+    }
   }
 }
 </script>
